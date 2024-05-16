b53995399aacea0f56310ccd603966442a30b155
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-05-16 13:51:54 +0800

    Add files via upload

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -6,6 +6,16 @@ const User = require('../schema/auth');
 
 const router = express.Router();
 
+
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
 // Signup route
 router.post('/signup', async (req, res) => {
   try {
