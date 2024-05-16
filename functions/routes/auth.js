ef99f66298b87e2e639e18ac47f332fe5ae9befc
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-05-16 13:23:59 +0800

    Add files via upload

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../schema/user');
+const User = require('../schema/auth');
 
 const router = express.Router();
 
