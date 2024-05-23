95d5bfb3d2ed6d6e91e37e16f53c945b6ba80bed
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-05-23 12:50:54 +0800

    Add files via upload

--- functions/routes/profile.js
+++ functions/routes/profile.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-// Route to handle updating a post
 router.put('/:userId', async (req, res) => {
     try {
         const userId = req.params.userId;
         const { username, email, number } = req.body;
         
-        // Find the post by ID and update its fields
         const updatedUser = await User.findByIdAndUpdate(userId, { username, email, number }, { new: true });
 
         res.json(updatedUser);
