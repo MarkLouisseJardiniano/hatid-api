5f8feaea49b82688ae50dcfd924b536e1e846792
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-05-28 11:19:21 +0800

    Add files via upload

--- functions/routes/enrollment.js
+++ functions/routes/enrollment.js
@@ -21,8 +21,8 @@ router.post('/signup', async (req, res) => {
         const { username, email, password, fullname, municipality, barangay, street, number, birthday } = req.body;
 
         // Check if the user already exists
-        let user = await Driver.findOne({ email });
-        if (user) {
+        let driver = await Driver.findOne({ email });
+        if (driver) {
             return res.status(400).json({ message: 'User already exists' });
         }
 
@@ -41,8 +41,8 @@ router.post('/signup', async (req, res) => {
             birthday,
         });
 
-        await user.save();
-        res.status(201).json({ message: 'User created successfully', username: user.username });
+        await driver.save();
+        res.status(201).json({ message: 'User created successfully', username: driver.username });
     } catch (error) {
         console.error('Error during signup:', error);
         res.status(500).json({ message: 'Server Error' });
@@ -79,20 +79,20 @@ router.post('/enroll-vehicle', async (req, res) => {
         const { userId, type, model, year, plateNumber } = req.body;
 
         // Find user by ID
-        const user = await Driver.findById(userId);
-        if (!user) {
+        const driver = await Driver.findById(userId);
+        if (!driver) {
             return res.status(404).json({ message: 'User not found' });
         }
 
         // Check if the plate number is already associated with another user
-        const existingVehicle = user.vehicles.find(vehicle => vehicle.plateNumber === plateNumber);
+        const existingVehicle = driver.vehicles.find(vehicle => vehicle.plateNumber === plateNumber);
         if (existingVehicle) {
             return res.status(400).json({ message: 'Plate number already registered' });
         }
 
         // Add the vehicle to the user's vehicles array
-        user.vehicles.push({ type, model, year, plateNumber });
-        await user.save();
+        driver.vehicles.push({ type, model, year, plateNumber });
+        await driver.save();
 
         res.status(201).json({ message: 'Vehicle enrolled successfully', vehicle: { type, model, year, plateNumber } });
     } catch (error) {
