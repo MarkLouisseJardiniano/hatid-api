d9a854e7c3a32cab331ab50b341963c82a60afa1
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-05-28 12:18:51 +0800

    Delete functions/models/enrollment.js

--- functions/models/enrollment.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const enrollmentSchema = require('../schema/enrollment');
-
-const DriverModel = mongoose.model('Driver', enrollmentSchema);
-
-module.exports = DriverModel;
